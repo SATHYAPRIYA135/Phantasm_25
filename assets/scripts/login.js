16055e28c417241914623a1ad2deb83a5a7adcfa
Logesh <lwi.heicho@gmail.com>
2025-01-03 21:26:33 +0530

    Js responsive script on styling the login box

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,13 +1,23 @@
 function switchLogin(){
     const box = document.getElementById("box");
     if(box.style.flexDirection == "row-reverse"){
+
         box.style.flexDirection = "row";
-        document.getElementById("login-title").innerHTML = "<p>LOGIN FOR PARTICIPANTS</p>"
-        document.getElementById("switch-title").innerHTML = "LOGIN FOR ADMINS"
+        document.getElementById("login-title").innerHTML = "<p>Login For Participants</p>";
+        document.getElementById("login").style.borderRadius = "8px 0px 0px 8px";
+        document.getElementById("switch-title").innerHTML = "Login For Admins";
+        document.getElementById("switch-title").style.rotate = "0deg";
+        document.getElementById("switch").style.borderRadius = "0px 8px 8px 0px";
+
     }else{
+
         box.style.flexDirection = "row-reverse";
-        document.getElementById("login-title").innerHTML = "<p>LOGIN FOR ADMINS</p>"
-        document.getElementById("switch-title").innerHTML = "LOGIN FOR PARTICIPANTS"
+        document.getElementById("login-title").innerHTML = "<p>Login For Admins</p>";
+        document.getElementById("login").style.borderRadius = "0px 8px 8px 0px";
+        document.getElementById("switch-title").innerHTML = "Login For Participants";
+        document.getElementById("switch-title").style.rotate = "180deg";
+        document.getElementById("switch").style.borderRadius = "8px 0px 0px 8px";
+
     }
 
 }
